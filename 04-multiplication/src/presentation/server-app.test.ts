869f53fd1b04ae15157fed2d53795a7a7ba3bda4
Luis Chilo <gfxargentina@gmail.com>
2024-02-27 18:49:31 -0300

    add unit test to server

--- 04-multiplication/src/presentation/server-app.test.ts
+++ 04-multiplication/src/presentation/server-app.test.ts
@@ -19,24 +19,50 @@ describe('Server App', () => {
   });
 
   it('should run server with options', () => {
-    const logSpy = jest.spyOn(console, 'log');
-    const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
-    const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+    //     //espia el console log
+    //     const logSpy = jest.spyOn(console, 'log');
+    //      //espia el caso de uso para crear archivos
+    //     const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
+    //      //espia el caso de uso para guardar archivos
+    //     const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+    //     Server.run(options);
+    //     //prueba que el console.log haya sido llamado 2 veces
+    //     expect(logSpy).toHaveBeenCalledTimes(2);
+    //     expect(logSpy).toHaveBeenCalledWith('Servidor Corriendo...');
+    //     expect(logSpy).toHaveBeenCalledWith('archivo creado');
+    //     //prueba los casos de uso hayan sido llamados
+    //     expect(createTableSpy).toHaveBeenCalledTimes(1);
+    //     expect(createTableSpy).toHaveBeenCalledWith({
+    //       base: options.base,
+    //       limit: options.limit,
+    //     });
+    //     expect(saveFileSpy).toHaveBeenCalledTimes(1);
+    //     expect(saveFileSpy).toHaveBeenCalledWith({
+    //       fileContent: expect.any(String),
+    //       fileDestination: options.fileDestination,
+    //       fileName: options.fileName,
+    //     });
+  });
+
+  it('should run with custom values mocked', () => {
+    const logMock = jest.fn();
+    const logErrorMock = jest.fn();
+    const createMock = jest.fn().mockReturnValue('1 x 2 = 2');
+    const saveFileMock = jest.fn();
+
+    console.log = logMock;
+    console.error = logErrorMock;
+    CreateTable.prototype.execute = createMock;
+    SaveFile.prototype.execute = saveFileMock;
 
     Server.run(options);
 
-    expect(logSpy).toHaveBeenCalledTimes(2);
-    expect(logSpy).toHaveBeenCalledWith('Servidor Corriendo...');
-    expect(logSpy).toHaveBeenCalledWith('archivo creado');
-
-    expect(createTableSpy).toHaveBeenCalledTimes(1);
-    expect(createTableSpy).toHaveBeenCalledWith({
+    expect(logMock).toHaveBeenCalledWith('Servidor Corriendo...');
+    expect(createMock).toHaveBeenCalledWith({
       base: options.base,
       limit: options.limit,
     });
-
-    expect(saveFileSpy).toHaveBeenCalledTimes(1);
-    expect(saveFileSpy).toHaveBeenCalledWith({
+    expect(saveFileMock).toHaveBeenCalledWith({
       fileContent: expect.any(String),
       fileDestination: options.fileDestination,
       fileName: options.fileName,
