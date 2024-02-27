beee9e1231c1548ee57e85a1c2cfdb062ed49ccf
Luis Chilo <gfxargentina@gmail.com>
2024-02-27 19:11:20 -0300

    test debug

--- 04-multiplication/src/presentation/server-app.test.ts
+++ 04-multiplication/src/presentation/server-app.test.ts
@@ -19,36 +19,36 @@ describe('Server App', () => {
   });
 
   it('should run server with options', () => {
-    //     //espia el console log
-    //     const logSpy = jest.spyOn(console, 'log');
-    //      //espia el caso de uso para crear archivos
-    //     const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
-    //      //espia el caso de uso para guardar archivos
-    //     const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
-    //     Server.run(options);
-    //     //prueba que el console.log haya sido llamado 2 veces
-    //     expect(logSpy).toHaveBeenCalledTimes(2);
-    //     expect(logSpy).toHaveBeenCalledWith('Servidor Corriendo...');
-    //     expect(logSpy).toHaveBeenCalledWith('archivo creado');
-    //     //prueba los casos de uso hayan sido llamados
-    //     expect(createTableSpy).toHaveBeenCalledTimes(1);
-    //     expect(createTableSpy).toHaveBeenCalledWith({
-    //       base: options.base,
-    //       limit: options.limit,
-    //     });
-    //     expect(saveFileSpy).toHaveBeenCalledTimes(1);
-    //     expect(saveFileSpy).toHaveBeenCalledWith({
-    //       fileContent: expect.any(String),
-    //       fileDestination: options.fileDestination,
-    //       fileName: options.fileName,
-    //     });
+    //espia el console log
+    const logSpy = jest.spyOn(console, 'log');
+    //espia el caso de uso para crear archivos
+    const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
+    //espia el caso de uso para guardar archivos
+    const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+    Server.run(options);
+    //prueba que el console.log haya sido llamado 2 veces
+    expect(logSpy).toHaveBeenCalledTimes(2);
+    expect(logSpy).toHaveBeenCalledWith('Servidor Corriendo...');
+    expect(logSpy).toHaveBeenCalledWith('archivo creado');
+    //prueba los casos de uso hayan sido llamados
+    expect(createTableSpy).toHaveBeenCalledTimes(1);
+    expect(createTableSpy).toHaveBeenCalledWith({
+      base: options.base,
+      limit: options.limit,
+    });
+    expect(saveFileSpy).toHaveBeenCalledTimes(1);
+    expect(saveFileSpy).toHaveBeenCalledWith({
+      fileContent: expect.any(String),
+      fileDestination: options.fileDestination,
+      fileName: options.fileName,
+    });
   });
 
   it('should run with custom values mocked', () => {
     const logMock = jest.fn();
     const logErrorMock = jest.fn();
     const createMock = jest.fn().mockReturnValue('1 x 2 = 2');
-    const saveFileMock = jest.fn();
+    const saveFileMock = jest.fn().mockReturnValue(false);
 
     console.log = logMock;
     console.error = logErrorMock;
@@ -67,5 +67,8 @@ describe('Server App', () => {
       fileDestination: options.fileDestination,
       fileName: options.fileName,
     });
+
+    //expect(logMock).toHaveBeenCalledWith('archivo creado');
+    expect(logErrorMock).not.toHaveBeenCalledWith();
   });
 });
