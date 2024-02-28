201a8f2226fc04adce53261db04311d08fa99e9b
Luis Chilo <gfxargentina@gmail.com>
2024-02-28 11:05:35 -0300

    add test app.ts

--- 04-multiplication/src/app.test.ts
+++ 04-multiplication/src/app.test.ts
@@ -1,5 +1,31 @@
-describe('App', () => {
-  test('should be true', () => {
-    expect(true).toBe(true);
+import { Server } from './presentation/server-app';
+
+describe('Test app.ts', () => {
+  test('should call Server.run with values', async () => {
+    const serverRunMock = jest.fn();
+    Server.run = serverRunMock;
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      '10',
+      '-l',
+      '5',
+      '-s',
+      '-n',
+      'test-file',
+      '-d',
+      'test-destination',
+    ];
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      showTable: true,
+      fileName: 'test-file',
+      fileDestination: 'test-destination',
+    });
   });
 });
