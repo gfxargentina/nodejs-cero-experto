2f0a180caf53d37f339b5d6e6fa5d7ee1f6e2a27
Luis Chilo <gfxargentina@gmail.com>
2024-04-30 11:51:29 -0300

    add mongo log datasource

--- 05-noc/src/domain/entities/log.entity.ts
+++ 05-noc/src/domain/entities/log.entity.ts
@@ -35,4 +35,16 @@ export class LogEntity {
 
     return log;
   };
+
+  static fromObject = (object: { [key: string]: any }): LogEntity => {
+    const { message, level, createdAt, origin } = object;
+    const logs = new LogEntity({
+      message,
+      level,
+      createdAt,
+      origin,
+    });
+
+    return logs;
+  };
 }
--- 05-noc/src/presentation/server.ts
+++ 05-noc/src/presentation/server.ts
@@ -1,12 +1,15 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log-datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email.service';
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDatasource()
+//aqui le decimos a la aplicacion donde guardar los logs
+const LogRepository = new LogRepositoryImpl(
+  //new FileSystemDatasource()
+  new MongoLogDatasource()
 );
 
 const emailService = new EmailService();
@@ -31,14 +34,14 @@ export class Server {
     //enviar email con attachements
     //emailService.sendEmailWithFileSystemLogs(['gfxargentina@gmail.com']);
 
-    // CronService.createJob('*/3 * * * * *', () => {
-    //   const url = 'https://google.com';
+    CronService.createJob('*/3 * * * * *', () => {
+      const url = 'https://tarjetasanimadas.com.ar';
 
-    //   new CheckService(
-    //     fileSystemLogRepository,
-    //     () => console.log(`${url} is up!!`),
-    //     (error) => console.log(error)
-    //   ).execute(url);
-    // });
+      new CheckService(
+        LogRepository,
+        () => console.log(`${url} is up!!`),
+        (error) => console.log(error)
+      ).execute(url);
+    });
   }
 }
